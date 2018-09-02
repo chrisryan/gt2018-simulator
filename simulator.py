cb0abf5367abbbbb3fa6ff4de882bc5c11bccb09
Chris Ryan <xgbe@yahoo.com>
2018-09-02 15:06:06 -0400

    Continued to work on layout of program.

--- simulator.py
+++ simulator.py
@@ -4,38 +4,68 @@ main = Tk()
 
 # Title
 titleText = StringVar()
-title = Label(main, textvariable=titleText, relief=RAISED, width=100, justify=LEFT)
-title.pack()
+title = Label(main, textvariable=titleText, relief=RAISED, justify=LEFT, font=("Arial Bold", 18))
+title.pack(fill="x", side=TOP)
 
 # Center
 frame = Frame(main)
-frame.pack(side=BOTTOM)
+frame.pack(side=TOP)
 
 statsCollection = Frame(frame)
 statsCollection.pack(side=LEFT)
 
 #  Row 1
-row1 = Frame(statsCollection)
-row1.pack(side=LEFT)
+row0Text = StringVar()
+row0TextLabel = Label(statsCollection, textvariable=row0Text, relief=RAISED, bd=10, width=4, height=2, bg="yellow", font=("Arial Bold", 50))
+row0TextLabel.grid(row=0, column=0);
+
+row0Graph = Canvas(statsCollection, relief=RAISED, width=712, height=178, bg="black")
+row0Graph.grid(row=0, column=1);
 
 row1Text = StringVar()
-row1TextLabel = Label(row1, textvariable=row1Text,relief=RAISED, width=5, bg="yellow")
-row1TextLabel.pack(side=LEFT);
+row1TextLabel = Label(statsCollection, textvariable=row1Text, relief=RAISED, bd=10, width=4, height=2, bg="blue", font=("Arial Bold", 50))
+row1TextLabel.grid(row=1, column=0);
 
-row1Graph = Canvas(row1, relief=RAISED, width=600, height=200, bg="black")
-row1Graph.pack(side=LEFT);
+row1Graph = Canvas(statsCollection, relief=RAISED, width=712, height=178, bg="black")
+row1Graph.grid(row=1, column=1);
+
+row2Text = StringVar()
+row2TextLabel = Label(statsCollection, textvariable=row2Text, relief=RAISED, bd=10, width=4, height=2, bg="red", font=("Arial Bold", 50))
+row2TextLabel.grid(row=2, column=0);
+
+row2Graph = Canvas(statsCollection, relief=RAISED, width=712, height=178, bg="black")
+row2Graph.grid(row=2, column=1);
+
+row3Text = StringVar()
+row3TextLabel = Label(statsCollection, textvariable=row3Text, relief=RAISED, bd=10, width=4, height=2, bg="pink", font=("Arial Bold", 50))
+row3TextLabel.grid(row=3, column=0);
+
+row3Graph = Canvas(statsCollection, relief=RAISED, width=712, height=178, bg="black")
+row3Graph.grid(row=3, column=1);
 
 
 visualCollection = Frame(frame)
 visualCollection.pack(side=LEFT)
 
-visual = Canvas(visualCollection, relief=RAISED, width=300, height=600, bg="green")
+visual = Canvas(visualCollection, relief=RAISED, width=474, height=712, bg="green")
 visual.pack(side=LEFT);
 
 # Bottom
+bottom = Frame(main)
+bottom.pack(side=TOP)
+
+timeText = StringVar()
+timeLabel = Label(bottom, textvariable=timeText, font=("Arial Bold", 36))
+timeLabel.pack()
+
 
 
 titleText.set("SUBJECT: SMITH, JOHN")
-row1Text.set("54")
+row0Text.set("54")
+row1Text.set("99")
+row2Text.set("12")
+row3Text.set("63")
+timeText.set("3:05 PM")
+
 
 main.mainloop()
