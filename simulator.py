669767d55063aea67526f3feb6bf5ee03f243753
Chris Ryan <xgbe@yahoo.com>
2018-09-07 20:36:31 -0400

    Added first class to replace rows and the random graph.

--- simulator.py
+++ simulator.py
@@ -3,6 +3,40 @@ from random import randint
 from datetime import datetime
 import time
 
+# Classes
+class GraphRow:
+    graphWidth = 712
+    graphHeight = 178
+
+    def __init__(self, parentWidget, rowNum):
+        self.value = StringVar()
+        self.label = Label(parentWidget, textvariable=self.value, relief=RAISED, bd=10, width=4, height=2, bg="black", font=("Arial Bold", 50))
+        self.graph = Canvas(parentWidget, relief=RAISED, width=self.graphWidth, height=self.graphHeight, bg="black")
+
+        self.label.grid(row=rowNum, column=0)
+        self.graph.grid(row=rowNum, column=1)
+
+class GraphRandom(GraphRow):
+    lastX = 0
+    lastY = 0
+    items = []
+    def update(self):
+        nextX = self.lastX + 10
+        if nextX > self.graphWidth:
+            nextX = 10
+            self.lastX = 0
+        nextY = randint(20, self.graphHeight - 20)
+        self.items.append(self.graph.create_line(self.lastX, self.lastY, nextX, nextY, fill="red"))
+
+        self.lastX = nextX
+        self.lastY = nextY
+
+        self.value.set(self.lastY)
+
+        if len(self.items) > 65:
+            self.graph.delete(self.items.pop(0))
+
+
 main = Tk()
 
 # Title
