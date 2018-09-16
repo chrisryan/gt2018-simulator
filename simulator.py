a7dea2f27bc6287e1c1b15723fbcf5d193ca4993
Chris Ryan <xgbe@yahoo.com>
2018-09-15 21:02:48 -0400

    Add visual background and brain sectors monitor.

--- simulator.py
+++ simulator.py
@@ -1,5 +1,7 @@
 from Tkinter import *
+from PIL import Image, ImageTk
 from datetime import datetime
+from random import randint
 import Graph
 
 main = Tk()
@@ -35,6 +37,40 @@ visualCollection.pack(side=LEFT)
 visual = Canvas(visualCollection, relief=RAISED, width=474, height=712, bg="green")
 visual.pack(side=LEFT);
 
+body = Image.open("images/person_background.png")
+bodyP = ImageTk.PhotoImage(body)
+visual.create_image(0, 0, anchor=NW, image=bodyP)
+
+brainBack = Image.open("images/brain_background.png")
+brainBackP = ImageTk.PhotoImage(brainBack)
+visual.create_image(10, 20, anchor=NW, image=brainBackP)
+
+brainSectors = []
+
+brainBlue = Image.open("images/brain_blue.png")
+brainBlueP = ImageTk.PhotoImage(brainBlue)
+brainSectors.append(visual.create_image(10, 20, anchor=NW, image=brainBlueP))
+
+brainPurple = Image.open("images/brain_purple.png")
+brainPurpleP = ImageTk.PhotoImage(brainPurple)
+brainSectors.append(visual.create_image(10, 20, anchor=NW, image=brainPurpleP))
+
+brainGreen = Image.open("images/brain_green.png")
+brainGreenP = ImageTk.PhotoImage(brainGreen)
+brainSectors.append(visual.create_image(10, 20, anchor=NW, image=brainGreenP))
+
+brainRed = Image.open("images/brain_red.png")
+brainRedP = ImageTk.PhotoImage(brainRed)
+brainSectors.append(visual.create_image(10, 20, anchor=NW, image=brainRedP))
+
+brainYellow = Image.open("images/brain_yellow.png")
+brainYellowP = ImageTk.PhotoImage(brainYellow)
+brainSectors.append(visual.create_image(10, 20, anchor=NW, image=brainYellowP))
+
+brainFront = Image.open("images/brain_foreground.png")
+brainFrontP = ImageTk.PhotoImage(brainFront)
+visual.create_image(10, 20, anchor=NW, image=brainFrontP)
+
 # Bottom
 bottom = Frame(main)
 bottom.pack(side=TOP)
@@ -44,10 +80,8 @@ timeLabel = Label(bottom, textvariable=timeText, font=("Arial Bold", 36))
 timeLabel.pack()
 
 
-
 titleText.set("SUBJECT: SMITH, JOHN")
 timeText.set("3:05 PM")
-
 
 def updateSimulation():
     # random
@@ -58,9 +92,16 @@ def updateSimulation():
 
     # bp graph
     bpGraph.update()
-    
+
     # wave
     waveGraph.update()
+
+    # brain sectors
+    sector = brainSectors[randint(0, 4)]
+    if randint(1, 3) == 1:
+        visual.itemconfig(sector, state=HIDDEN)
+    else:
+        visual.itemconfig(sector, state=NORMAL)
 
     # Time box
     timeText.set(datetime.now().time())
