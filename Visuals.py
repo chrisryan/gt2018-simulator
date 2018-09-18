d7db3aa2950785a715d947103a74fd922d43b595
Chris Ryan <xgbe@yahoo.com>
2018-09-17 20:50:30 -0400

    Refactor brain code into separate class.

--- Visuals.py
+++ Visuals.py
@@ -7,7 +7,6 @@ class Visuals:
     visualsWidth = 474
     visualsHeight = 712
     sourceImages = []
-    brainSectors = []
 
     def __init__(self, parentWidget):
         visualCollection = Frame(parentWidget)
@@ -18,16 +17,7 @@ class Visuals:
 
         self.loadImage("images/person_background.png", 0, 0)
 
-        self.loadImage("images/brain_background.png", 10, 20)
-
-        self.brainSectors.append(self.loadImage("images/brain_blue.png", 10, 20))
-        self.brainSectors.append(self.loadImage("images/brain_green.png", 10, 20))
-        self.brainSectors.append(self.loadImage("images/brain_red.png", 10, 20))
-        self.brainSectors.append(self.loadImage("images/brain_yellow.png", 10, 20))
-        self.brainSectors.append(self.loadImage("images/brain_purple.png", 10, 20))
-
-        self.loadImage("images/brain_foreground.png", 10, 20)
-
+        self.brain = BrainVisual(self)
 
     def loadImage(self, filename, x, y):
         image = Image.open(filename)
@@ -36,9 +26,28 @@ class Visuals:
         return self.visual.create_image(x, y, anchor=NW, image=imageP)
 
     def update(self):
+        self.brain.update()
+
+class BrainVisual:
+    brainSectors = []
+
+    def __init__(self, visuals):
+        self.visuals = visuals
+
+        visuals.loadImage("images/brain_background.png", 10, 20)
+
+        self.brainSectors.append(visuals.loadImage("images/brain_blue.png", 10, 20))
+        self.brainSectors.append(visuals.loadImage("images/brain_green.png", 10, 20))
+        self.brainSectors.append(visuals.loadImage("images/brain_red.png", 10, 20))
+        self.brainSectors.append(visuals.loadImage("images/brain_yellow.png", 10, 20))
+        self.brainSectors.append(visuals.loadImage("images/brain_purple.png", 10, 20))
+
+        visuals.loadImage("images/brain_foreground.png", 10, 20)
+
+    def update(self):
         # brain sectors
         sector = self.brainSectors[randint(0, 4)]
         if randint(1, 3) == 1:
-            self.visual.itemconfig(sector, state=HIDDEN)
+            self.visuals.visual.itemconfig(sector, state=HIDDEN)
         else:
-            self.visual.itemconfig(sector, state=NORMAL)
+            self.visuals.visual.itemconfig(sector, state=NORMAL)
